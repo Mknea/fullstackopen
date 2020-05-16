1e2832543c024915e55a10ea5e60e483d885c87c
Mknea <aleksimatias.makinen@gmail.com>
2020-05-16 13:37:47 +0300

    2.11: Fetch data in start from db.json

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,17 +1,11 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 import FilterForm from './components/filterForm'
 import PersonForm from './components/personForm'
 import Persons from './components/persons'
 
 const App = () => {
-  const [ persons, setPersons] = useState(
-    [
-      { name: 'Arto Hellas', number: '040-123456' },
-      { name: 'Ada Lovelace', number: '39-44-5323523' },
-      { name: 'Dan Abramov', number: '12-43-234345' },
-      { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]
-  ) 
+  const [ persons, setPersons] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber] = useState('')
   const [ filterName, setFilterName] = useState('')
@@ -28,6 +22,14 @@ const App = () => {
     console.log(event.target.value)
     setNewNumber(event.target.value)
   }
+
+  useEffect( () => {
+    axios
+      .get('http://localhost:3001/db')
+      .then(response => {
+        setPersons(response.data.persons)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
