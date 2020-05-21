2b01ddf6f2f8f67969b6f27cb98e24aac07173e9
Mknea <aleksimatias.makinen@gmail.com>
2020-05-21 16:19:23 +0300

    2.19-20: More refined alerts and errors

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -3,12 +3,16 @@ import FilterForm from './components/filterForm'
 import PersonForm from './components/personForm'
 import Persons from './components/persons'
 import personService from './services/persons'
+import Notification from './components/notification'
 
 const App = () => {
   const [ persons, setPersons] = useState([]) 
-  const [ newName, setNewName ] = useState('')
-  const [ newNumber, setNewNumber] = useState('')
-  const [ filterName, setFilterName] = useState('')
+  const [ newName, setNewName ] = useState('') // Text input in name form
+  const [ newNumber, setNewNumber] = useState('') // Text input in number form
+  const [ filterName, setFilterName] = useState('') // Text input in filter form
+
+  const [ notification, setNotification] = useState(null)
+  const [ isNotificationError, setIsNotificationError] = useState(true)
 
   const handleFilterChange = (event) => {
     setFilterName(event.target.value)
@@ -26,7 +30,16 @@ const App = () => {
       .then(initialPersons => {
         setPersons(initialPersons)
       })
-  }, [])
+  }, []) // Perform at start
+
+  const displayNotification = (text, isError) => {
+    console.log(text)
+    setNotification(text)
+    setIsNotificationError(isError)
+    setTimeout(() => {
+      setNotification(null)
+    }, 5000)
+  }
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -43,8 +56,12 @@ const App = () => {
             setPersons(persons.map(person => 
               person.id === returnedPerson.id ? returnedPerson : person)
             )
+            displayNotification(`${returnedPerson.name}'s number was succesfully modified.`, false)
           })
-          .catch(error => window.alert(`Error updating the existing entry: ${error}`))
+          .catch((error) => {
+            setPersons(persons.filter(person => person.id !== changedPerson.id))
+            displayNotification(`Information of ${changedPerson.name} has already been removed from server`, true)
+          })
       }
     }
     else {
@@ -56,10 +73,11 @@ const App = () => {
         .create(newPerson)
         .then(addedPerson => {
           setPersons(persons.concat(addedPerson))
+          displayNotification(`${addedPerson.name} was succesfully added.`, false)
           setNewName('')
           setNewNumber('')
         })
-        .catch(error => window.alert(`Error adding a new entry: ${error}`))
+        .catch(error => displayNotification(`Error encountered while adding a new person`, true))
     }
   }
   const removePerson = (personToBeDeleted) => {
@@ -68,16 +86,25 @@ const App = () => {
       .remove(personToBeDeleted)
       .then(() => {
         setPersons(persons.filter(person => person.id !== personToBeDeleted.id))
+        displayNotification(`${personToBeDeleted.name} was succesfully removed.`, false)
       })
-      .catch(error => window.alert(`Error removing the existing entry: ${error}`))
+      .catch(error => {
+        setPersons(persons.filter(person => person.id !== personToBeDeleted.id))
+        displayNotification(`Information of ${personToBeDeleted.name} has already been removed from server`, true)
+      })
     }
   }
+
   const personsToShow = persons.filter(person =>
     person.name.toLowerCase().includes(filterName.toLowerCase())
   )
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification
+        text={notification}
+        isError={isNotificationError}
+      />
       <FilterForm 
         name={filterName}
         handler={handleFilterChange}
