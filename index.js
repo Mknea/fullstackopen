e7d7b02867b5136b9466c09f79e159a6f33e7113
Mknea <aleksimatias.makinen@gmail.com>
2020-05-27 13:43:19 +0300

    3.4: Specific resource delete Yksilöitävä DELETE toimii

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const phonebook = [
+let phonebook = [
     { 
     "name": "Arto Hellas", 
     "number": "040-123456",
@@ -22,37 +22,50 @@ const phonebook = [
     "number": "39-23-6423122",
     "id": 4
     }
-]
+];
 
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to phonebook</h1>')
+    res.send('<h1>Welcome to phonebook</h1>');
 })
   
 app.get('/api/persons', (req, res) => {
-    res.json(phonebook)
+    res.json(phonebook);
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = phonebook.find(person => person.id === id)
+    const id = Number(req.params.id);
+    const person = phonebook.find(person => person.id === id);
     if (person) {
-        res.json(person)
+        res.json(person);
     }
     else {
-        res.status(404).end()
+        res.status(404).end();
     }
 })
 
 app.get('/info', (req, res) => {
     const timestamp = new Date();
-    return(
-        res.send(
-            `<div>Phonebook has info for ${phonebook.length} people</div>` +
-            `<div>${timestamp}</div>`)
-    )
+    res.send(
+        `<div>Phonebook has info for ${phonebook.length} people</div>` +
+        `<div>${timestamp}</div>`
+    );
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const personIndex = phonebook.findIndex(person => person.id === id);
+    console.log(`Request to delete index ${personIndex}`);
+    if (personIndex !== -1) {
+        console.log('Found! Deleting');
+        phonebook.splice(personIndex, 1);
+        res.status(200).end();
+    }
+    else {
+        res.status(404).end();
+    }
 })
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 })
