fc017f155cfe1c302db892c046b199840d43d684
Mknea <aleksimatias.makinen@gmail.com>
2020-06-02 14:48:38 +0300

    uibuild

---
+++ build/precache-manifest.85d8a7cb53c75571993277fde7380977.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e33d0ae74cd0deb6c8297c6dd39dcc91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5683e5449f5b28e2fa95",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "35404b6f8ba79f06ae8b",
+    "url": "/static/js/2.666133f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.666133f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5683e5449f5b28e2fa95",
+    "url": "/static/js/main.29654beb.chunk.js"
+  },
+  {
+    "revision": "d6d4691db26ca793a81a",
+    "url": "/static/js/runtime-main.4f3ee9ca.js"
+  }
+]);
