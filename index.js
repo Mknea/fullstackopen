4b9ecf2a00f5a6f275e3db17725916fb0fd95f7b
Mknea <aleksimatias.makinen@gmail.com>
2021-04-10 22:14:32 +0300

    3.13-3.15 (3.18): Use db for all operations
    Henkilötiedot haetaan ja tallennetaan mongoDB.
    Myös niiden poisto, ja id:llä haku & /INFO-endpoint
    toteutettu.
    Tässä vaiheessa ollaan välittämättä siitä, onko
    tietokannassa jo henkilöä jolla on sama nimi
    kuin lisättävällä.

--- index.js
+++ index.js
@@ -1,115 +1,114 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-var morgan = require('morgan')
+const morgan = require('morgan')
 const cors = require('cors')
+
+const Person = require('./models/person')
 
 app.use(express.static('build'))
 app.use(cors())
 app.use(express.json())
 
 morgan.token('postData', (req, res) => {
-    if (req.method === 'POST') {
-        return JSON.stringify(req.body);
-    }
-    return "";
+	if (req.method === 'POST') {
+		return JSON.stringify(req.body);
+	}
+	return "";
 })
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
 
-let phonebook = [
-    { 
-    "name": "Arto Hellas", 
-    "number": "040-123456",
-    "id": 1
-    },
-    { 
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523",
-    "id": 2
-    },
-    { 
-    "name": "Dan Abramov", 
-    "number": "12-43-234345",
-    "id": 3
-    },
-    { 
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122",
-    "id": 4
-    }
-];
-
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome to phonebook</h1>');
+	res.send('<h1>Welcome to phonebook</h1>');
 })
-  
+
 app.get('/api/persons', (req, res) => {
-    res.json(phonebook);
+	Person
+		.find()
+		.then(persons => {
+			res.json(persons)
+		})
 })
 
 app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const person = phonebook.find(person => person.id === id);
-    if (person) {
-        res.json(person);
-    }
-    else {
-        res.status(404).end();
-    }
+	Person
+		.findById(req.params.id)
+		.then(person => {
+			if (person) {
+				res.json(person);
+			}
+			else {
+				res.status(404).end();
+			}
+		});
 })
 
 app.get('/info', (req, res) => {
-    const timestamp = new Date();
-    res.send(
-        `<div>Phonebook has info for ${phonebook.length} people</div>` +
-        `<div>${timestamp}</div>`
-    );
+	const timestamp = new Date();
+	Person
+		.find()
+		.countDocuments((err, count) => {
+			if (err) {
+				res.status(500).end();
+			}
+			else {
+				res.send(
+					`<div>Phonebook has info for ${count} people</div>` +
+					`<div>${timestamp}</div>`
+				);
+			}
+    });
 })
 
 app.post('/api/persons', (req, res) => {
-    const newPerson = req.body;
-    //console.log(`Received POST for person: ${JSON.stringify(newPerson)}`);
-    const hasValidFields =  newPerson.hasOwnProperty('name') &&
-                            newPerson.hasOwnProperty('number');
-    const isNew = (!phonebook.some(person => person.name === newPerson.name));
-    if (hasValidFields && isNew) {
-        if (newPerson.name === "" || newPerson.number === "") {
-            return res.status(400).json({error: 'Content missing'});
-        }
-        const newID = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 4) + 4); // 4 first entries are hardcoded
-        const addedPerson = {...newPerson, "id": newID};
-        phonebook.push(addedPerson);
-        res.json(addedPerson);
-    }
-    else if (hasValidFields) 
-        res.status(409).json({error: 'Entry already exists'});
-    else                
-    {
-        res.status(400).json({error: 'Malformed request'});
-    }
+	const newPerson = req.body;
+	const hasValidFields = newPerson.hasOwnProperty('name') &&
+		newPerson.hasOwnProperty('number');
+	//const isNew = (!phonebook.some(person => person.name === newPerson.name));
+	if (hasValidFields) { //&& isNew) {
+		if (newPerson.name === "" || newPerson.number === "") {
+			return res.status(400).json({ error: 'Content missing' });
+		}
+		//const newID = Math.floor(Math.random() * (Number.MAX_SAFE_INTEGER - 4) + 4); // 4 first entries are hardcoded
+		//const addedPerson = { ...newPerson, "id": newID };
+		const person = new Person({
+			name: newPerson.name,
+			number: newPerson.number
+		})
+		person
+			.save()
+			.then(savedPerson => {
+				console.log(
+					`Saved ${savedPerson.name} number ${savedPerson.number} to db with id ${savedPerson._id}`)
+				res.json(savedPerson);
+			})		
+	}
+	//else if (hasValidFields)
+	//	res.status(409).json({ error: 'Entry already exists' });
+	else {
+		res.status(400).json({ error: 'Malformed request' });
+	}
 })
 
 app.delete('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id);
-    const personIndex = phonebook.findIndex(person => person.id === id);
-    //console.log(`Request to delete index ${personIndex}`);
-    if (personIndex !== -1) {
-        //console.log('Found! Deleting');
-        phonebook.splice(personIndex, 1);
-        res.status(200).end();
-    }
-    else {
-        res.status(404).end();
-    }
+	Person.deleteOne({_id: req.params.id}).then(result => {
+		if (result.deletedCount === 1) {
+			res.status(200).end();
+		}
+		else {
+			res.status(404).end();
+		}
+	});
 })
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
-  
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
 app.use(unknownEndpoint)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 })
